ec21dde0bfe243dc1a13e72ebe57681b4ba5f534
Olena <7890572@ukr.net>
2023-03-16 17:31:23 +0100

    Added a file radio.js

--- radio.js
+++ radio.js
@@ -1,20 +1,18 @@
+'use-strict'
+
 const inputNumber = document.querySelector('.number')
 const radioUp = document.querySelector('.up');
-const radioDown = document.querySelector('.down')
 
 const form  = document.querySelector('.form').addEventListener('submit', (event) => {
     event.preventDefault();
     const inputValue = inputNumber.value.split(', ');
-    arrNumber = inputValue.map((number => Number(number)));
-    const radioUpSort = radioUp.checked;
-    const radiDownSort = radioDown.checked;
-    if(radioUpSort){
+    let arrNumber = inputValue.map((number => Number(number)));
+    const radioSort = radioUp.checked;
+    if(radioSort){
         arrNumber.sort((a, b) => {
             return a - b;
         }); 
-    }
-    if(radiDownSort){
-        arrNumber.sort((a, b) => {
+    } else {arrNumber.sort((a, b) => {
             return b - a;
         }); 
     }
